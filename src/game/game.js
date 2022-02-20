e9a1798fa074bc4d64ee60748c40660477249fe0
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-02-19 20:42:27 -0800

    added Game logic

--- src/game/game.js
+++ src/game/game.js
@@ -1,9 +1,40 @@
-
+import { useState, useEffect } from 'react'
+import './game.css'
 const Game = (props) => {
 
+    const [cards, setCards] = useState([]);
+
+    
+    const createCards = () => {
+        for(let i = 0; i < 16; i++) {
+            const card = {number: i, isClicked: false}
+            const addCard = (card) => setCards(state => [...state, card])
+            addCard(card)
+        }
+    };
+    
+
+    useEffect(() => {
+        createCards();
+    }, [])
+
+    const testEffect = (e) => {
+        e.preventDefault()
+        console.log('worked')
+        console.log(cards)
+    }
+
+    const displayCards = cards.map((card) => 
+        <div key={card.number}>Card</div>
+    );
+
+
     return(
-        <div>
-            <button onClick={props.buttonClick}>Click</button>
+        <div id='gameContainer'>
+            <div id='cardsContainer'>
+                {displayCards}
+                <button onClick={testEffect}>test</button>
+            </div>
         </div>
     )
 }
